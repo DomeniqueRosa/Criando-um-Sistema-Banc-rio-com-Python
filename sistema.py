5857455dbd0cb3ffba1d940d35cd3ed7d570d795
Alires Rosa <alires.cardoso@ufpr.br>
2024-04-25 20:57:47 -0300

    Adicionado mais as funcoes de criar contas, usuarios e listar contas

--- sistema.py
+++ sistema.py
@@ -6,6 +6,9 @@ Digite opção desejada:
 [1] - Depositar
 [2] - Saque
 [3] - Extrato
+[4] - Criar usuario
+[5] - Criar conta
+[6] - Listar contas
 [0] - Sair
 
 **********************************
@@ -16,6 +19,41 @@ LIMITE_MAXIMO = 500
 tentativas_saque_no_dia = 0
 saldo_da_conta = 0
 extrato = []
+contas = []
+usuarios = []
+contas = []
+def listar_contas(contas):
+    for c in contas:
+        for chave, valor in c.items():
+            print(f"{chave} : {valor}")
+           
+def filtrar_usuario(cpf, usuarios):
+    ind = -1
+    for u in usuarios:
+        ind += 1
+        if cpf in u["cpf"]:
+            esta = True
+            
+            break
+        else:
+            esta = False
+
+    return esta, ind
+
+def criar_conta(contas, usuario, ind, tamanho):
+    numero_conta = tamanho + 1
+    conta = {"nr_conta" : numero_conta, "agencia" : "0001", "usuario": usuario[ind]}
+    return conta
+
+def criar_usuario():
+    nome = input("Digite o nome: ")
+    data = input("Digite data de nascimento dd-mm-aaaa: ")
+    cpf = input("Digite o cpf do usuario: ")
+    endereco = input("Digite o endereço rua, nro - bairro - cidade/estado:  ")
+
+    cadastro = {"nome": nome, "data": data, "cpf": cpf, "endereco": endereco}
+
+    return cadastro
 
 def depositar (saldo_da_conta, valor, extrato):    
     if(valor > 0):
@@ -37,7 +75,7 @@ def sacar(saldo_da_conta=None, valor_saque=None, extrato=None, limite=LIMITE_MAX
             print("Valor maior que disponivel em conta, saque cancelado.")
         if(valor_saque > LIMITE_MAXIMO):
             print("Limite ultrapassado, é permitido saque apenas de valores de R$500,00 ou menor!")
-        if(tentativas_saque_no_dia > LIMITE_MAXIMO):
+        if(tentativas_saque_no_dia > LIMITE_TENTATIVAS_SAQUE):
             print("Excedito tentativas de saque por dia!")
     return saldo_da_conta, extrato, tentativas_saque_no_dia
 
@@ -65,7 +103,25 @@ while(True):
         saldo_da_conta, extrato, tentativas_saque_no_dia = sacar(saldo_da_conta= saldo_da_conta, valor_saque= valor_saque, extrato= extrato, limite=LIMITE_MAXIMO,tentativas_saque_no_dia=tentativas_saque_no_dia)     
     elif(opcao == 3):
        extrato_conta(saldo_da_conta, extrato)
-       input("Digite Enter para continuar.")     
+       input("Digite Enter para continuar.")
+    elif(opcao == 4):
+        cpf = input("Digite o cpf apenas com numeros: ")
+        esta, ind = filtrar_usuario(cpf, usuarios)
+        if(esta):
+            print("Usuario já cadastrado!")
+        else:
+            usuarios.append(criar_usuario())
+    elif(opcao == 5):
+        cpf = input("Digite o cpf apenas com numeros: ")
+        esta, ind = filtrar_usuario(cpf, usuarios)
+        if(esta):
+            tamanho = len(contas)
+            contas.append(criar_conta(cpf, usuarios, ind, tamanho))
+            print("Cadastrado realizado com sucesso!")
+        else:
+            print("Usuario não cadastrado, para criar conta o cpf deve estar cadastrado!")
+    elif(opcao == 6):
+        listar_contas(contas)
     elif(opcao == 0):
         break
     else:
